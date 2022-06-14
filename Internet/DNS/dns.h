44f9e22c94e582ee8bbc7c81b85623e089d95221
James <javakys@github.com>
2022-06-14 19:59:06 +0900

    Debug Option Disabled

--- Internet/DNS/dns.h
+++ Internet/DNS/dns.h
@@ -60,7 +60,7 @@ extern "C" {
  * @brief Define it for Debug & Monitor DNS processing.
  * @note If defined, it dependens on <stdio.h>
  */
-#define _DNS_DEBUG_
+//#define _DNS_DEBUG_
 
 #define	MAX_DNS_BUF_SIZE	256		///< maximum size of DNS buffer. */
 /*
